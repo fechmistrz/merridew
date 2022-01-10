ac805e16f9d99d57a2b8bbe7eee731ecd3b91057
Remigiusz Suwalski <remigiusz.suwalski@gmail.com>
2022-01-10 15:32:08 +0100

    Add verification of mandatory fields in .bib file

--- bibliography_sort.py
+++ bibliography_sort.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 import re
 
+
 def strip_year(text):
     """Filter non-digit characters and convert to int"""
 
@@ -46,19 +47,19 @@ def validate_authors(authors):
         if not re.match(regex, author):
             raise ValueError(f"Incorrect author syntax '{author}' in '{authors}'")
     return
-    
+
 
 def parse_bib(input_bib_file):
     """Converts .bib file into list of entries"""
     entries = dict()
-    accepted_line = re.compile('^( *[A-Z]+ *= *\{.*\},|^@[a-z]+ \{[a-z]+_?[0-9]+,|\}|\s*)$')
+    accepted_line = re.compile("^( *[A-Z]+ *= *\{.*\},|^@[a-z]+ \{[a-z]+_?[0-9]+,|\}|\s*)$")
 
     with open(input_bib_file, "r") as bib_file:
         for raw_line in bib_file:
             line = raw_line.strip()
             if not accepted_line.match(line):
                 raise NotImplementedError("Syntax error in .bib file: " + line)
-                
+
             match = re.search(" *@([^ ]+) *{ *([^,]+),", line)
             if match:
                 # when line is: "@article {alexander28,"
@@ -68,7 +69,7 @@ def parse_bib(input_bib_file):
 
                 entries[bibtex_name] = {
                     "bibtex_type": bibtex_type,
-                    "bibtex_name": bibtex_name
+                    "bibtex_name": bibtex_name,
                 }
 
             match = re.search(" *([a-zA-Z]+) *= *(.*)", line)
@@ -88,15 +89,55 @@ def parse_bib(input_bib_file):
 def bibliography_sort(input_bib_file):
     """Main function sorting bibliography entries"""
 
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     entries = parse_bib(input_bib_file)
     entries = sorted(list(entries.values()), key=sort_key)
     with open(input_bib_file, "w") as output_bib_file:
         output_bib_file.write(print_nicely(entries))
 
 
+def check_mandatory_optional(input_bib_file):
+    """Looks for missing mandatory or illegal optional fields"""
+
+    entry_type = "@unknown"
+    entry_name = "unknown80"
+    field = "???"
+    fields = list()
+
+    correct_values = {
+        "@article": ["author", "title", "journal", "year"],
+        "@book": ["author", "title", "publisher", "year"],
+        "@incollection": ["author", "title", "booktitle", "publisher", "year"],
+        "@inproceedings": ["author", "title", "booktitle", "year"],
+        "@misc": [],
+        "@phdthesis": ["author", "title", "school", "year"],
+    }
+
+    with open(input_bib_file, "r") as bib_file:
+        for raw_line in bib_file:
+            line = raw_line.strip()
+
+            if line.startswith("@"):
+                entry_type, entry_name = line.replace("{", "").replace(",", "").split()
+                if entry_type not in correct_values.keys():
+                    raise Exception(f"ENTRY TYPE {entry_type}")
+
+            elif " = " in line:
+                field = line.split(" = ")[0].lower()
+                fields.append(field)
+
+            elif line.startswith("}"):
+                missing = [f for f in correct_values[entry_type] if f not in fields]
+                if missing:
+                    raise Exception(f"Missing {missing} in {entry_name}!")
+                fields = list()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--bib", help="Path to the bibliography file", required=True)
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
     bibliography_sort(args.bib)
+    check_mandatory_optional(args.bib)
