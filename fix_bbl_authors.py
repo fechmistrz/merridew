3cfc1c5b9d1bb420b6f5d0bc5f78b07661476362
Leo Suwalski <104537982+fechmistrz@users.noreply.github.com>
2024-02-16 18:57:43 +0100

    Translate vol, no, phd thesis and pages from English to Polish

--- fix_bbl_authors.py
+++ fix_bbl_authors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This script opens .bbl file and replaces 'and' with ',' in authors
+This script modifies .bbl file in place, imitating English-Polish translation
 """
 
 import os
@@ -11,34 +11,56 @@ import sys
 
 def find_replace(input_file):
     """Fix 'authors' entries"""
-    bibitem_line = False
     new_content = list()
 
     with open(input_file, "r") as f:
-        for line in f:
-            new_line = line
-            if bibitem_line:
-                new_line = new_line.replace(", and ", ", ")
-                new_line = new_line.replace(" and ", ", ")
+        is_authors_line = False
+        for raw_line in f:
+            line = raw_line
 
-            if new_line != line:
-                logging.warning("Changing '{}' => '{}'".format(line.strip(), new_line.strip()))
+            replacements = [
+                ('(.*)(pages )([0-9]+--[0-9]+)(.*)', r'\1strony \3\4'),
+                ('(.*)(volume[~ ])([0-9]+ )of (.*)', r'\1tom~\3\4'),
+                ('(.*), volume~([0-9]+),(.*)', r'\1, tom~\2,\3'),
+                ('(.*), No. ([0-9]+[.]?)$', r'\1, numer \2'),
+                ('(.*), Vol. ([0-9]+)(.*)', r'\1, tom \2\3'),
+                ('^(.newblock) In (.*)', r'\1 W \2'),
+                ('^(.newblock) (PhD thesis)(.*)', r'\1 Praca doktorska\3'),
+            ]
+            for r in replacements:
+                new_line = re.sub(r[0], r[1], line)
+                if line != new_line:
+                    logging.warning(f"Changing: {line.strip()} -> {new_line.strip()}")
+                    line = new_line
 
-            new_content.append(new_line)
+            if re.search(r"^\\bibitem.*", line):
+                is_authors_line = True
+                authors = ""
+                new_content.append(line)
+                continue
 
-            match = re.search(r"^\\bibitem.*", line)
-            if match:
-                bibitem_line = True
+            if re.search(r"^\\newblock.*", line):
+                if is_authors_line:
+                    new_content.append(" ".join(authors.strip().split()).replace(", and ", " oraz ").replace(" and ", " oraz ") + "\n")
+                    is_authors_line = False
+
+            if is_authors_line:
+                authors += line.strip() + " "
             else:
-                bibitem_line = False
+                new_content.append(line.strip())
+
 
     with open(input_file, "w") as f:
-        for line in new_content:
-            f.write(line)
+        new_content = " ".join(new_content)
+        while "  " in new_content:
+            new_content = new_content.replace("  ", " ")
+        new_content = new_content.replace(" \\newblock ", "\n\\newblock ").replace(" \\bibitem", "\n\\bibitem").replace("\n ", "\n").replace("\n\n", "\n").replace("\\bibitem", "\n\\bibitem")
+        f.write(new_content)
 
     return
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING, format="%(levelname)s: %(message)s")
+    find_replace(sys.argv[1])
     find_replace(sys.argv[1])
